41681f26db42ac7e9924c97d4b9e4657f7d77aa8
Pritam Sarkar <001pritam2012@gmail.com>
2016-05-15 01:16:16 +0530

    Add event emitter program

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 console.log("Hello, World!")
 
 var http = require("http");
@@ -10,6 +11,7 @@ http.createServer(function(request, response){
 
 console.log("Server running at http://127.0.0.1:8081");
 
+// ---------------------------------------------------
 var fs = require("fs");
 
 var data = fs.readFileSync('input.txt');
@@ -25,3 +27,26 @@ fs2.readFile('input.txt', function(err, data){
 
 console.log("Program async last command executed!");
 
+*/
+// ---------------------------------------------------
+var event1 = require("events");
+
+var eventEmitter1 = new event1.EventEmitter();
+
+// Create an event handler as follows
+var connectHandler = function connected() {
+   console.log('connection succesful.');
+  
+   // Fire the data_received event 
+   eventEmitter1.emit('data_received');
+}
+
+eventEmitter1.on('connection', connectHandler);
+
+eventEmitter1.on('data_received', function(){
+	console.log('data received successfully');
+});
+
+eventEmitter1.emit('connection');
+
+console.log('Event program ended!');
