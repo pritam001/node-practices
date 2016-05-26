0fa9b5da8a1df99eb036fb7c69b9386bb1cc01f8
Pritam Sarkar <001pritam2012@gmail.com>
2016-05-26 21:08:18 +0530

    Start node buffers

--- practice_tutorialspoint/main.js
+++ practice_tutorialspoint/main.js
@@ -6,7 +6,7 @@ var http = require("http");
 http.createServer(function(request, response){
 	// Send the http header, HTTP Status : 200 OK
 	response.writeHead(200,{'Content-Type':'text/plain'});
-	response.end('SCREW YOU !!!! \nMAGIC Samyak');
+	response.end('Hello World!');
 }).listen(8081);
 
 console.log("Server running at http://127.0.0.1:8081");
