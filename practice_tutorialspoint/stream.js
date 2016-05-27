52d18dbc50b76fd17ee5cfb0c5bf82f51cb5a744
Pritam Sarkar <001pritam2012@gmail.com>
2016-05-28 01:23:31 +0530

    Write stream

--- practice_tutorialspoint/stream.js
+++ practice_tutorialspoint/stream.js
@@ -20,4 +20,28 @@ readerStream.on('error', function(err){
    console.log(err.stack);
 });
 
-console.log("Program Ended");
+console.log("Read Program Ended");
+
+
+var fs = require("fs");
+var data2 = 'Simply Easy Learning';
+
+// Create a writable stream
+var writerStream = fs.createWriteStream('output.txt');
+
+// Write the data to stream with encoding to be utf8
+writerStream.write(data2,'UTF8');
+
+// Mark the end of file
+writerStream.end();
+
+// Handle stream events --> finish, and error
+writerStream.on('finish', function() {
+    console.log("Write completed.");
+});
+
+writerStream.on('error', function(err){
+   console.log(err.stack);
+});
+
+console.log("Write Program Ended");
