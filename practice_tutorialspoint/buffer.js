f2117674079b37be9b9780a693a2290ff6b0fbc1
Pritam Sarkar <001pritam2012@gmail.com>
2016-05-27 19:43:36 +0530

    Slice Buffer

--- practice_tutorialspoint/buffer.js
+++ practice_tutorialspoint/buffer.js
@@ -43,3 +43,8 @@ var buffer1 = new Buffer('ABC');
 var buffer2 = new Buffer(3);
 buffer1.copy(buffer2);
 console.log("buffer2 content: " + buffer2.toString());
+
+var buffer1 = new Buffer('TutorialsPoint');
+//slicing a buffer
+var buffer2 = buffer1.slice(0,9);
+console.log("buffer2 content: " + buffer2.toString());
