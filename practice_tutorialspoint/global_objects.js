ec17ec75f97810d5b675eab972fbc52d67e3fcff
Pritam Sarkar <001pritam2012@gmail.com>
2016-05-28 13:23:13 +0530

    set and clear Timeout

--- practice_tutorialspoint/global_objects.js
+++ practice_tutorialspoint/global_objects.js
@@ -1,1 +1,11 @@
 console.log( __filename );
+
+console.log( __dirname );
+
+function printHello(){
+   console.log( "Hello, World!");
+}
+// Now call above function after 2 seconds
+var t = setTimeout(printHello, 2000);
+
+clearTimeout(t);
